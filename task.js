36057f397bbef718b498633761d8e466379672fe
Evgeniy Chausov <evgeniy@chausov.ru>
2024-03-28 08:42:44 +0300

    final HW

--- task.js
+++ task.js
@@ -1,27 +1,34 @@
 const siginBtn = document.getElementById('signin__btn');
 const form = document.forms.signin__form;
 const xhr = new XMLHttpRequest();
+const ls = localStorage.getItem('id');
+
+if (ls) {
+  document.querySelector('.signin').classList.remove('signin_active');
+  document.querySelector('.welcome').classList.add('welcome_active');
+
+  document.getElementById('user_id').textContent = ls;
+} 
 
 siginBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
-  xhr.addEventListener('load', () => {
-    const response = xhr.response;
-    if (response['success']) {
-      form.reset();
-      document.querySelector('.signin').classList.remove('signin_active');
-      document.querySelector('.welcome').classList.add('welcome_active');
-      
-      localStorage.setItem('id', response['user_id']);
-      document.getElementById('user_id').textContent = response['user_id'];
-    } else {
-      alert('Неверный логин/пароль');
+  xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+      const response = JSON.parse(xhr.responseText);
+      if (response['success']) {
+        document.querySelector('.signin').classList.remove('signin_active');
+        document.querySelector('.welcome').classList.add('welcome_active');
+
+        localStorage.setItem('id', response['user_id']);
+        document.getElementById('user_id').textContent = response['user_id'];
+      } else {
+        alert('Неверный логин/пароль');
+      }
     }
   });
 
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
   const formData = new FormData(form);
-  xhr.responseType = 'json';
   xhr.send(formData);
 });
-
